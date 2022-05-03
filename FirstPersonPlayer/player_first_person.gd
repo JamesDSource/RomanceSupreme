6cd067c7c97895654278329878d37c8e6b3919ab
James Duran <Jamesevora367@gmail.com>
2022-05-02 20:56:51 -0700

    Added first person controller

--- FirstPersonPlayer/player_first_person.gd
+++ FirstPersonPlayer/player_first_person.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-const MAX_SPEED = 5.0
-const ACCELERATION = 0.2
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -15,23 +15,27 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		camera_pivot.rotation.y -= event.relative.x*0.001
-		camera_verticle_rot = clampf(camera_verticle_rot - event.relative.y*0.001, -.5, .5)
+		camera_verticle_rot = clampf(camera_verticle_rot - event.relative.y*0.001, -deg2rad(90.0), deg2rad(90.0))
 		camera_pivot.rotation.x = camera_verticle_rot
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity.4
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * MAX_SPEED
-		velocity.z = direction.z * MAX_SPEED
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
-		velocity.z = move_toward(velocity.z, 0, MAX_SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 	move_and_slide()
