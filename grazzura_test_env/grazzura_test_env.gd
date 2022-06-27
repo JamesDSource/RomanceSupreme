745452e7d086bf7fad88b95886bb76eaffdc4840
Grazurra <65730132+Grazurra@users.noreply.github.com>
2022-06-27 15:27:46 -0400

    lil push

--- grazzura_test_env/grazzura_test_env.gd
+++ grazzura_test_env/grazzura_test_env.gd
@@ -1,7 +1,13 @@
+
+
 extends Spatial
 
 func _ready():
-	SequenceManager.start_seq("scene1Wakeup", $CutsceneCameras.get_children(), funcref(self, "_on_sequence_finished"))
+	SequenceManager.start_seq("scene1Wakeup", $CutsceneCameras.get_children(), funcref(self, "_on_sequence_finished"), funcref(self, "_on_sequence_event"))
 
 func _on_sequence_finished(custom_vars):
 	print(custom_vars)
+
+func _on_sequence_event(event: String, custom_vars: Dictionary, done: FuncRef):
+	print(event)
+	done.call_func()
